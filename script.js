34dde6dddf7b28339b64b2f99aef0779e7e35ddd
Valerie Russell <vruss14@gmail.com>
2022-01-18 18:44:14 -0500

    all GDP data scaled to fit on screen; create x and y axis

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
 .then(response => response.json())
-.then(data => {
-    console.log(data);
+.then(resp => {
+    createChart(resp.data)
 })
+
+function createChart(dataset) {
+    console.log(dataset);
+
+    const w = 900;
+    const h = 700;
+    const padding = 60;
+
+    // Min in x direction is year 1947; parseInt returns correct value
+    // Max in x direction is year 2015; parseInt returns 2015 from the date
+    const xScale = d3.scaleLinear()
+        .domain([d3.min(dataset, (d) => parseInt(d[0])), d3.max(dataset, (d) => parseInt(d[0]))])
+        .range([padding, w - padding]);
+
+    const yScale = d3.scaleLinear()
+        .domain([0, d3.max(dataset, (d) => d[1])])
+        .range([h - padding, padding]);
+
+    // Make the SVG with width 900 & height 700
+    const svg = d3.select("#light-container")
+        .append("svg")
+        .attr("width", w)
+        .attr("height", h)
+        .attr('id', 'title');
+
+    // Create a bar for each data point
+    // x position = index * 3 + padding
+    // y position = 0.03 scale to fit all data points on screen
+
+    svg.selectAll("rect")
+        .data(dataset)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => (i * 2.84) + padding)
+        .attr("y", (d, i) => h - (0.03 * d[1]) - padding)
+        .attr("width", 2)
+        .attr("height", (d, i) => d[1] * 0.03)
+        .attr("fill", "lightblue")
+        .attr("class", "bar")
+
+
+    // Create and append X and Y axes
+
+    const xAxis = d3.axisBottom(xScale);
+    const yAxis = d3.axisLeft(yScale);
+
+    svg.append("g")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .attr('id', 'x-axis')
+    .call(xAxis);
+
+    svg.append("g")
+    .attr("transform", "translate(" + padding + ",0)")
+    .attr('id', 'y-axis')
+    .call(yAxis)
+}
+
