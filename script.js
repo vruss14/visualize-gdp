b7cadb2aadede073ffa6a059025d17f5e2b5b26c
Valerie Russell <vruss14@gmail.com>
2022-01-18 19:09:44 -0500

    add tooltip for bars onmouseover

--- script.js
+++ script.js
@@ -14,38 +14,52 @@ function createChart(dataset) {
     // Min in x direction is year 1947; parseInt returns correct value
     // Max in x direction is year 2015; parseInt returns 2015 from the date
     const xScale = d3.scaleLinear()
-        .domain([d3.min(dataset, (d) => parseInt(d[0])), d3.max(dataset, (d) => parseInt(d[0]))])
-        .range([padding, w - padding]);
+    .domain([d3.min(dataset, (d) => parseInt(d[0])), d3.max(dataset, (d) => parseInt(d[0]))])
+    .range([padding, w - padding]);
 
     const yScale = d3.scaleLinear()
-        .domain([0, d3.max(dataset, (d) => d[1])])
-        .range([h - padding, padding]);
+    .domain([0, d3.max(dataset, (d) => d[1])])
+    .range([h - padding, padding]);
 
     // Make the SVG with width 900 & height 700
     const svg = d3.select("#light-container")
-        .append("svg")
-        .attr("width", w)
-        .attr("height", h)
-        .attr('id', 'title');
+    .append("svg")
+    .attr("width", w)
+    .attr("height", h)
+    .attr('id', 'title');
+
+    const toolTip = d3.select('#light-container').append('div')
+    .attr('id', 'tooltip')
+    .style('opacity', 0)
 
     // Create a bar for each data point
     // x position = index * 3 + padding
     // y position = 0.03 scale to fit all data points on screen
 
     svg.selectAll("rect")
-        .data(dataset)
-        .enter()
-        .append("rect")
-        .attr("x", (d, i) => (i * 2.84) + padding)
-        .attr("y", (d, i) => h - (0.03 * d[1]) - padding)
-        .attr("width", 2)
-        .attr("height", (d, i) => d[1] * 0.03)
-        .attr("fill", "lightblue")
-        .attr("class", "bar")
-
+    .data(dataset)
+    .enter()
+    .append("rect")
+    .attr("x", (d, i) => (i * 2.84) + padding)
+    .attr("y", (d, i) => h - (0.03 * d[1]) - padding)
+    .attr("width", 2)
+    .attr("height", (d, i) => d[1] * 0.03)
+    .attr("fill", "lightblue")
+    .attr("class", "bar")
+    .attr("data-date", (d) => `${d[0]}`)
+    .attr("data-gdp", (d) => `${d[1]}`)
+    .on('mouseover', function(event) {
+        toolTip.style('opacity', 1)
+        toolTip.html(event.target.__data__[1])
+        toolTip.style('left', 300)
+        toolTip.style('top', 300)
+        toolTip.attr('data-date', event.target.__data__[0])
+    })
+    .on('mouseout', function () {
+        toolTip.style('opacity', 0)
+    })
 
     // Create and append X and Y axes
-
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
 
